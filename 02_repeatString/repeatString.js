f93a2a4410623c163b259c9016bd6b0dfe26b9a3
JOASH BRAHMA <joashbrahma552@gmail.com>
2024-04-11 23:29:14 +0530

    Add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    if (num < 0) return "ERROR";
+    if (str === "") return str;
 
+    let newStr = "";
+
+    while (num > 0) {
+        newStr += str;
+        num--;
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
