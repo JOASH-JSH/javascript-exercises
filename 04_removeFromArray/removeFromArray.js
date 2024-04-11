a8676403e4324f4f25c27f7cd4f984ce8e1218af
JOASH BRAHMA <joashbrahma552@gmail.com>
2024-04-12 00:04:12 +0530

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (nums, ...targets) {
+    targets.forEach((target) => {
+        nums = nums.filter((num) => (num !== target ? num : 0));
+    });
+    return nums;
 };
 
 // Do not edit below this line
