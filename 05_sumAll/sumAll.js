f180299b9847c0094e6e82389788ef687a9c78a3
JOASH BRAHMA <joashbrahma552@gmail.com>
2024-04-12 09:47:37 +0530

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+function checkIsItNumberType(param) {
+    return typeof param === "number" ? true : false;
+}
 
+const sumAll = function (start, end) {
+    if (!checkIsItNumberType(start) || !checkIsItNumberType(end)) {
+        return "ERROR";
+    }
+    
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
