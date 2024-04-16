0ad8c9460b1abee5d6f3755a5f5f877b08b41f55
JOASH BRAHMA <joashbrahma552@gmail.com>
2024-04-16 16:10:38 +0530

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+function isValidChar(char) {
+    const charCode = char.charCodeAt();
+    if (
+        (charCode >= 48 && charCode <= 57) ||
+        (charCode >= 65 && charCode <= 90) ||
+        (charCode >= 97 && charCode <= 122)
+    ) return true;
+    return false;
+}
 
+const palindromes = function (str) {
+    const newStr = str.toLowerCase();
+    let start = 0;
+    let end = newStr.length - 1;
+
+    while (start <= end) {
+        while (!isValidChar(newStr[start])) {
+            start++;
+        }
+        while (!isValidChar(newStr[end])) {
+            end--;
+        }
+
+        if (newStr[start] !== newStr[end]) return false;
+
+        start++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
