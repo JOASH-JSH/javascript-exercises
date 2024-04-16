176004d2915a564a3d387bbe4d7c5c8151e39051
JOASH BRAHMA <joashbrahma552@gmail.com>
2024-04-16 18:41:40 +0530

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = parseInt(n);
 
+    if (n < 0) return "OOPS";
+    if (n === 1) return 1;
+
+    let prevSum = 0, curSum = 1;
+
+    for (let i = 0; i < n; i++) {
+        prevSum += curSum;
+        curSum = prevSum - curSum;
+    }
+
+    return prevSum;
 };
 
 // Do not edit below this line
